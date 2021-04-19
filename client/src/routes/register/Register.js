14d42b7c6e2e40f011baf4ba2e24b469f0e52343
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-04-19 18:47:51 -0500

    front register progress

--- client/src/routes/register/Register.js
+++ client/src/routes/register/Register.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Link from 'react-router-dom'
+
+const initialRegisterValues = {
+    username: '',
+    password: '',
+    first_name: '',
+    last_name: '',
+    email: ''
+}
+
 
 const Register = () => {
+    const [register, setRegister] = useState(initialRegisterValues)
+
+    const handleChange = event => {
+        setRegister({...register, [event.target.name] : event.target.value})
+    }
+    
+    const handleSubmit = event => {
+        event.preventDefault()
+
+    }
+
+    const isValid = register.username === '' || register.password === '' || register.first_name === '' || register.last_name === '' || register.email === ''
+
     return (
-        <div>
-            
-        </div>
+        <section>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Username:</label>
+                    <input type="text" name="username" id="username" value={register.username} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="name">Password:</label>
+                    <input type="text" name="password" id="password" value={register.password} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="name">First Name:</label>
+                    <input type="text" name="first_name" id="last_name" value={register.first_name} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="name">Last Name:</label>
+                    <input type="text" name="last_name" id="first_name" value={register.last_name} onChange={handleChange} />
+                </div>
+                <div>
+                    <button disabled={isValid}>Register</button>
+                    <Link to="">Already have an account? Log In</Link>
+                </div>
+            </form>
+        </section>
     )
 }
 
