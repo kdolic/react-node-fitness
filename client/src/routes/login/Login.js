ad8ee42a0d752ea4e86163fd88828a554f70b42a
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-04-19 18:41:20 -0500

    backend deploy issue/front login progress

--- client/src/routes/login/Login.js
+++ client/src/routes/login/Login.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Link from 'react-router-dom'
+
+const initialLoginValues = {
+    username: '',
+    password: ''
+}
 
 const Login = () => {
+    const [login, setLogin] = useState(initialLoginValues)
+
+    const handleChange = event => {
+        setLogin({...login, [event.target.name] : event.target.value})
+    }
+    
+    const handleSubmit = event => {
+        event.preventDefault()
+
+    }
+
+    const isValid = login.username === '' || login.password === ''
+
     return (
-        <div>
-            
-        </div>
+        <section>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Username:</label>
+                    <input type="text" name="username" id="username" value={login.username} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="name">Password:</label>
+                    <input type="text" name="password" id="password" value={login.password} onChange={handleChange} />
+                </div>
+                <div>
+                    <button disabled={isValid}>Log In</button>
+                    <Link to="">Don't have an account? Register</Link>
+                </div>
+            </form>
+        </section>
     )
 }
 
