99c664da058c61243ab066d5b8ee5ed86595c36c
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-04-22 15:47:30 -0500

    banner wip

--- client/src/components/home/Banner.js
+++ client/src/components/home/Banner.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Carousel} from 'react-bootstrap'
+import banner1 from '../../assets/images/dbs.jpg'
+import banner2 from '../../assets/images/nutrition.jpg'
+import banner3 from '../../assets/images/fitness-model.jpg'
 
 const Banner = () => {
     return (
-        <header>
-            
-        </header>
+       <div className="header">
+           <img src={banner1} alt=""/>
+       </div>
+       // Carousel system displaying three different banners (5s)
     )
 }
 
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Banner from './Banner'
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Banner />
         </div>
     )
 }
