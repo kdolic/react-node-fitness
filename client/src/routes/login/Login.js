58fdddb32a2f42e1b662f4f94f7aaaea08aa4970
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-04-28 17:16:11 -0500

    login progress

--- client/src/routes/login/Login.js
+++ client/src/routes/login/Login.js
@@ -1,16 +1,38 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import * as yup from "yup";
+import formSchema from "../../validation/formSchema";
+import register1 from "../../assets/images/register1.jpg";
 
 const initialLoginValues = {
     username: '',
     password: ''
 }
 
+const initialLoginErrors = {
+    username: "",
+    password: ""
+}
+
 const Login = () => {
     const [login, setLogin] = useState(initialLoginValues)
+    const [loginErrors, setLoginErrors] = useState(initialLoginErrors)
 
     const handleChange = event => {
         setLogin({...login, [event.target.name] : event.target.value})
+
+    yup
+      .reach(formSchema, event.target.name)
+      .validate(event.target.value)
+      .then(() => {
+        setLoginErrors({ ...loginErrors, [event.target.name]: "" });
+      })
+      .catch((error) => {
+        setLoginErrors({
+          ...loginErrors,
+          [event.target.name]: error.errors[0],
+        });
+      });
     }
     
     const handleSubmit = event => {
@@ -21,20 +43,62 @@ const Login = () => {
     const isValid = login.username === '' || login.password === ''
 
     return (
-        <section>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Username:</label>
-                    <input type="text" name="username" id="username" placeholder="Username..." value={login.username} onChange={handleChange} />
+        <section className='container'>
+            <img className="loginImg" src={register1} alt="loginImg" />
+            <form className="loginForm" onSubmit={handleSubmit}>
+            <div className="loginTop">
+                <h1>DevFit</h1>
+                {isValid ? (
+            <img
+              className="lockImg"
+              src="https://img.icons8.com/fluent/48/000000/lock.png"
+              alt="lockLogo"
+            />
+          ) : (
+            <img
+              className="lockImg"
+              src="https://img.icons8.com/fluent/48/000000/approved-unlock.png"
+              alt="lockLogo"
+            />
+          )}
+
+          <h3>Login</h3>
+          </div>
+          <div>
+          <div className="error">{loginErrors.username}</div>
+                    <input 
+                      type="text" 
+                      name="username" 
+                      id="username" 
+                      placeholder="Username *" 
+                      value={login.username} 
+                      onChange={handleChange} 
+                    />
                 </div>
                 <div>
-                    <label htmlFor="name">Password:</label>
-                    <input type="text" name="password" id="password" placeholder="Password..." value={login.password} onChange={handleChange} />
+                <div className="error">{loginErrors.password}</div>
+                    <input 
+                       type="text" 
+                       name="password" 
+                       id="password" 
+                       placeholder="Password *" 
+                       value={login.password} 
+                       onChange={handleChange} 
+                    />
                 </div>
-                <div>
-                    <button disabled={isValid}>Log In</button>
-                    <Link to="/register">Don't have an account? Register</Link>
-                </div>
+                <div className="registerButton">
+          {isValid ? (
+            <button disabled={isValid}>Fill All Fields</button>
+          ) : (
+            <button disabled={isValid} onSubmit={handleSubmit}>
+              Login
+            </button>
+          )}
+          {/* <button disabled={isValid}>Register</button> */}
+          <Link className="link" to="/register">
+            Don't have an account? Register
+          </Link>
+        </div>
             </form>
         </section>
     )
