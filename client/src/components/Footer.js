08ec8223ecbff275464d043491c45bc66131f477
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-04-30 11:33:52 -0500

    footer progress

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,54 +1,57 @@
 import React from "react";
+import {Nav} from "react-bootstrap";
 import "../css/Footer.css";
 
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div className='left'>
-                <ul>
-                    <li>Solutions</li>
-                    <li>Marketing</li>
-                    <li>Analytics</li>
-                    <li>Commerce</li>
-                    <li>Insights</li>
-                </ul>
+            <Nav className='footer-navbar'>
+                    <Nav.Link className='footer-nav' href="/">Home</Nav.Link>
+                    <Nav.Link className='footer-nav' href="#">Features</Nav.Link>
+                    <Nav.Link className='footer-nav' href="#">About</Nav.Link>
+                    <Nav.Link className='footer-nav' href="#">Gallery</Nav.Link>
+                    <Nav.Link className='footer-nav' href="#">Team</Nav.Link>
+                
+                {/* <hr/> */}
+            </Nav>
+            <Nav className='left'> 
+                <Nav.Link className='support' href='#'>SUPPORT</Nav.Link>
+                <Nav.Link className='support' href='#'>Pricing</Nav.Link>
+                <Nav.Link className='support' href='#'>Guides</Nav.Link>
+                <Nav.Link className='support' href='#'>Training</Nav.Link>
+                <Nav.Link className='support' href='#'>Customer Service</Nav.Link>
                 <br/>
-                <ul>
-                    <li>Members</li>
-                    <li>The Dev.Fit App</li>
-                    <li>Store</li>
-                    <li>FAQS</li>
-                </ul>
+                <Nav.Link className='members' href='#'>MEMBERS</Nav.Link>
+                <Nav.Link className='members' href='#'>The DEV.FIT App</Nav.Link>
+                <Nav.Link className='members' href='#'>Store</Nav.Link>
+                <Nav.Link className='members' href='#'>FAQS</Nav.Link>
                 <br/>
-            </div>
+            </Nav>
             <div className='middle'>
-
+                <h1>LOGO</h1>
+                <p>Develop Yourself, Develop Your Fit</p>
             </div>
-            <div className='right'>
-                <ul>
-                    <li>Legal</li>
-                    <li>Claim</li>
-                    <li>Privacy</li>
-                    <li>Terms</li>
-                </ul>
+            <Nav className='right'>
+                <Nav.Link className='legal' href='#'>LEGAL</Nav.Link>
+                <Nav.Link className='legal' href='#'>Claim</Nav.Link>
+                <Nav.Link className='legal' href='#'>Privacy</Nav.Link>
+                <Nav.Link className='legal' href='#'>Terms</Nav.Link>
                 <br/>
-                <ul>
-                    <li>Support</li>
-                    <li>Pricing</li>
-                    <li>Guides</li>
-                    <li>Training</li>
-                    <li>Customer Service</li>
-                </ul>
-            </div>
+                <Nav.Link className='solutions' href='#'>SOLUTIONS</Nav.Link>
+                <Nav.Link className='solutions' href='#'>Marketing</Nav.Link>
+                <Nav.Link className='solutions' href='#'>Analytics</Nav.Link>
+                <Nav.Link className='solutions' href='#'>Commerce</Nav.Link>
+                <Nav.Link className='solutions' href='#'>Insights</Nav.Link>
+            </Nav>
             <div className='bottom'>
                 <hr/>
                 <p>© DEV.FIT, Inc. All rights reserved.</p>
                 <div className='social'>
-                    <div class="icons8-facebook"></div>
-                    <div class="icons8-twitter"></div>
-                    <div class="icons8-instagram"></div>
-                    <div class="icons8-play-button"></div>
-                    <div class="icons8-tiktok"></div>
+                    <div className="icons8-facebook"></div>
+                    <div className="icons8-twitter"></div>
+                    <div className="icons8-instagram"></div>
+                    <div className="icons8-play-button"></div>
+                    <div className="icons8-tiktok"></div>
                 </div>
             </div>
         </footer>
