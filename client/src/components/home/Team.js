e3aa8433b33707c2987306d857df9dd25ff1e577
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-05-03 21:33:33 -0500

    team progress

--- client/src/components/home/Team.js
+++ client/src/components/home/Team.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../css/Team.css'
 
 const Team = () => {
     return (
@@ -12,13 +13,13 @@ const Team = () => {
                     <p>IMAGE HERE</p>
                     <h3>Kenan Dolic</h3>
                     <p>Role</p>
-                    <button className='team-btn'>Find Out More</button>
+                    <a href='https://www.linkedin.com/in/kenan-dolic/' target='blank'><button className='team-btn'>Find Out More</button></a>
                 </div>
                 <div>
                     <p>IMAGE HERE</p>
                     <h3>Daniel Wright</h3>
                     <p>Role</p>
-                    <button className='team-btn'>Find Out More</button>
+                    <a href='https://www.linkedin.com/in/daniel-wright-1207/' target='blank'><button className='team-btn'>Find Out More</button></a>
                 </div>
             </div>
         </section>
