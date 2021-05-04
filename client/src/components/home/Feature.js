74f536f8b6b763d413e2f0a28dfab6c884bcfee7
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-05-04 18:54:05 -0500

    features started

--- client/src/components/home/Feature.js
+++ client/src/components/home/Feature.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import '../../css/Feature.css';
 
 const Feature = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='featContainer'>
+            <div className='title'>
+                <h1>Fea<span className='title-span gradient'>tu</span>res</h1>
+            </div>
+            <div className='features'>
+                <div className='feature'>
+                    <h1>ICON</h1>
+                    <h3>Exercise</h3>
+                    <p></p>
+                </div>
+                <div className='feature'>
+                    <h1>ICON</h1>
+                    <h3>Fasting</h3>
+                    <p></p>
+                </div>
+                <div className='feature'>
+                    <h1>ICON</h1>
+                    <h3>Goals</h3>
+                    <p></p>
+                </div>
+                <div className='feature'>
+                    <h1>ICON</h1>
+                    <h3>Streak</h3>
+                    <p></p>
+                </div>
+            </div>
+        </section>
     )
 }
 
