5fb029d83730f16cb9a9f9695b5ce661995ae83e
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-05-17 15:22:43 -0500

    update about component styling

--- client/src/components/home/About.js
+++ client/src/components/home/About.js
@@ -9,14 +9,13 @@ const About = () => {
                 <div className='aboutTitle'>
                     <h1>Abou<span className='title-span gradient'>t D</span>ev.Fit</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <hr />
                 </div>
                 <div className='aboutContent'>
                     <img src={aboutUs} alt="aboutImg" width='300rem'/>
                     <div className='aboutSection'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <p>Welcome to Dev.Fit. <br /> Develop Yourself, Develop Your Fit</p>
+                        <p>Welcome to DEV.FIT. <br /><span className='slogan'>Develop Yourself, Develop Your Fit</span></p>
                     </div>
                 </div>
                 <div className='aboutCards'>
@@ -25,17 +24,21 @@ const About = () => {
                         <Row>
                             <Col className='card'>
                                 <h3>Why DEV.FIT? <img src="https://img.icons8.com/ios-glyphs/30/fa314a/long-arrow-right.png" alt='right_arrow' /></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                             </Col>
                             <Col className='card'>
                                 <h3>Inquries <img src="https://img.icons8.com/ios-glyphs/30/fa314a/long-arrow-right.png" alt='right_arrow' /></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                             </Col>
                         </Row>
                         <Row>
                             <Col className='card'>
                                 <h3>DEV.FIT Development <img src="https://img.icons8.com/ios-glyphs/30/fa314a/long-arrow-right.png" alt='right_arrow' /></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                             </Col>
                             <Col className='card'>
                                 <h3>The DEV.FIT Mobile App <img src="https://img.icons8.com/ios-glyphs/30/fa314a/long-arrow-right.png" alt='right_arrow' /></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                             </Col>
                         </Row>
                     </Container>
