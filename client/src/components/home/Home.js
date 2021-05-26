8bac3b19ff51712f552a63d4440b3f3b2c2b7871
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-05-26 15:46:07 -0500

    started banner component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -10,7 +10,7 @@ import Footer from '../Footer';
 const Home = () => {
     return (
         <div className='home'>
-            {/* <Banner/> */}
+            <Banner/>
             <Feature/>
             <About/>
             <Gallery/>
