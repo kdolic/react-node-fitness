0a5f9e73a9edee218174f81ec0c70aaf8712df1f
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-06-02 16:56:37 -0500

    updated banner component w react-slick

--- client/src/components/home/Banner.js
+++ client/src/components/home/Banner.js
@@ -1,47 +1,35 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import '../../css/Banner.css';
 import banner1 from "../../assets/images/fasting.jpg";
 import banner2 from "../../assets/images/nutrition.jpg";
-import banner3 from "../../assets/images/fitness-model.jpg";
+import banner3 from "../../assets/images/1banner.jpg";
 
 const Banner = () => {
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000
+  }
+
   return (
-    <Carousel className='bannerContainer' activeIndex='0' fade >
-      <Carousel.Item interval={1000}>
-        <img
-          className="first"
-          src={banner1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="second"
-          src={banner2}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="third"
-          src={banner3}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-</Carousel>
+    <Slider className='bannerContainer' {...settings}>
+      <div className='imgContainer'>
+        <img src={banner1} alt='banner1' />
+      </div>
+      <div className='imgContainer'>
+        <img src={banner2} alt='banner2' />
+      </div>
+      <div className='imgContainer'>
+        <img src={banner3} alt='banner3' />
+      </div>
+    </Slider>
   );
 };
 
