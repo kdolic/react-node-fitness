eb1e4a8b0c2c583545066c83a3c6866885d4cce1
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-06-29 17:02:51 -0500

    updated banner component media query styling

--- client/src/components/home/Banner.js
+++ client/src/components/home/Banner.js
@@ -3,9 +3,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../../css/Banner.css';
-import banner1 from "../../assets/images/fasting.jpg";
+import banner3 from "../../assets/images/fasting.jpg";
 import banner2 from "../../assets/images/nutrition.jpg";
-import banner3 from "../../assets/images/gym.jpg";
+import banner1 from "../../assets/images/gym.jpg";
 
 const Banner = () => {
 
@@ -19,17 +19,17 @@ const Banner = () => {
   }
 
   return (
-    <Slider className='bannerContainer' {...settings}>
-      <div className='imgContainer'>
-        <img src={banner1} alt='banner1' />
-      </div>
-      <div className='imgContainer'>
-        <img src={banner2} alt='banner2' />
-      </div>
-      <div className='imgContainer'>
-        <img src={banner3} alt='banner3' />
-      </div>
-    </Slider>
+      <Slider className='bannerContainer' {...settings}>
+        <div className='imgContainer'>
+          <img src={banner1} alt='banner1' />
+        </div>
+        <div className='imgContainer'>
+          <img src={banner2} alt='banner2' />
+        </div>
+        <div className='imgContainer'>
+          <img src={banner3} alt='banner3' />
+        </div>
+      </Slider>
   );
 };
 
